191d1496ef058fdcf46e0255fb74605e5760a920
Rafael de Mello Ferreira <rafaelmfer@hotmail.com>
2024-11-22 13:45:05 -0800

    [EC-358, EC-359] Put Validation of Token in the Endpoints and Fix Vision Prescription (#19)
    * feat: [EC-358] enable validation of token in the endpoints
    
    * fix: [EC-359] adjust endpoint to save vision prescription

--- index.js
+++ index.js
@@ -34,28 +34,6 @@ mongoose
         console.error(err);
     });
 
-// JWT setup
-app.use((req, res, next) => {
-    if (
-        req.headers &&
-        req.headers.authorization &&
-        req.headers.authorization.split("=")[0] === "JWT"
-    ) {
-        jsonwebtoken.verify(
-            req.headers.authorization.split("=")[1],
-            process.env.SECRET_JWT_SIGN,
-            (err, decode) => {
-                if (err) req.user = undefined;
-                req.user = decode;
-                next();
-            }
-        );
-    } else {
-        req.user = undefined;
-        next();
-    }
-});
-
 // Routes
 // Basic route
 app.get("/", (req, res) => {
